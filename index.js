e82a02b502481656789d5e16af1595f3a4dca0b5
phydoz <phydoz5@gmail.com>
2020-02-03 10:56:23 +0300

    Ajout fonctionnement mot de passe et email dans le fichier index.js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const pwd = document.querySelector('#password');
 const mail = document.querySelector('#email');
 
-console.log(pwd, mail);
+mail.addEventListener('input', (e)=>{
+    const email = e.target.value;
+    const format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(email.length>0) {
+        if(email.match(format)) {
+            mail.style.borderColor = "green";
+        } else {
+            mail.style.borderColor = "red";
+        }
+    } else {
+        mail.style.borderColor = "black";
+    }    
+});
+pwd.addEventListener('input', (e)=>{
+    const mdp = e.target.value;
+    const format = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
+
+    if(mdp.length>0) {
+        if(mdp.match(format)) {
+            pwd.style.borderColor = "green";
+            console.log("test ici ok")
+        } else {
+            pwd.style.borderColor = "red";
+            console.log("test ici pas ok")
+        }
+    } else {
+        pwd.style.borderColor = "black";
+        console.log("test 0 ok")
+    }    
+});
